4296b696581e318fdebd659a96642f106f7580a8
lmh <hugue_liu@yahoo.com>
2019-02-20 17:01:38 +0800

    strange-advertising

---
+++ strange-advertising.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the viralAdvertising function below.
+int viralAdvertising(int n) {
+    int thiscount = 5, sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += thiscount/2;
+        thiscount = thiscount / 2 * 3;
+    }
+    return sum;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
